30f331c514b0c77a8ef09bdde1c6d5d4a300ca83
Marni <83829559+Flakvard@users.noreply.github.com>
2023-05-16 10:28:33 +0200

    Update top_k_frequent_elements.cpp

--- Arrays_&_Hashing/top_k_frequent_elements.cpp
+++ Arrays_&_Hashing/top_k_frequent_elements.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <queue>
 
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        std::map <int,vector<int>> mp;
-        for(auto x:nums){
-            int i = x;
-            mp[i].push_back(x);
-        }
-        vector<int> topKfrequent;
-        int top = k;
-        sort(mp.begin(),mp.end());
+        std::map<int,int>mp; // key values and counts of values
+
+        std::priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>q;
+        //Use greater<> to change `pq` into a min heap priority queue
+        //Pair/tuple the values in a vector to push from map counts
         
-        for(int i = 0; i < k; i++){
-            topKfrequent.push_back(mp[i]);
+  
+        for(int num:nums){
+            mp[num]++; // add num as a key value and count each key value
+        }
+        for(auto count:mp){
+            q.push({count.second, count.first}); 
+            // push second val(lowest counts) as first priority in queue
+            if(q.size() > k) q.pop(); // remove top count(lowest) when above k
+        }
+
+        vector<int>topKfrequent;
+        
+        while(k--){
+            topKfrequent.push_back(q.top().second); 
+            // push top second val(key value, not counts) 
+            q.pop(); // remove top value
         }
 
         return topKfrequent;
