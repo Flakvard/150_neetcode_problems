fed3dd24b58e5827c146acece130ecfb522965ec
Marni <83829559+Flakvard@users.noreply.github.com>
2023-05-17 23:16:11 +0200

    Update product_of_array_except_self.cpp
    Did not pass all tests. 18/25, too slow algorithm. Time limit exceeded.

--- Arrays_&_Hashing/product_of_array_except_self.cpp
+++ Arrays_&_Hashing/product_of_array_except_self.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        // Input: nums = [1,2,3,4]
+        // Output:nums = [24,12,8,6]
+        // 2*3*4 = 24
+        // 1*3*4 = 12
+        // 1*2*4 = 8
+        // 1*2*3 = 6
 
+        int n = nums.size();
+        std::vector<int>arr;
+        std::priority_queue<int, std::vector<int>>pg;
+        std::vector<int>res;
+
+        for(int i = 0; i < n; i++){
+            for(int j=0; j < n; j++){
+                if(i==j){
+
+                }else{
+                    int newNum = nums[j]; 
+                    pg.push(newNum);
+                }
+            }
+            int arrSize = pg.size();
+            int sum = 1;
+
+            for(int k=0; k < arrSize; k++){
+                sum *= pg.top();
+                pg.pop();
+            }
+            arr.push_back(sum);
+        }
+        return arr;
+    }
+};
