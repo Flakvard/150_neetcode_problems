782a2642351de47f81e27f4bf3d2daa37fb58808
Marni <83829559+Flakvard@users.noreply.github.com>
2023-05-20 18:45:12 +0200

    Update product_of_array_except_self.cpp
    Passed all test. Dynamic Programming ( Tabulation )

--- Arrays_&_Hashing/product_of_array_except_self.cpp
+++ Arrays_&_Hashing/product_of_array_except_self.cpp
@@ -10,27 +10,42 @@ public:
         // 1*2*4 = 8
         // 1*2*3 = 6
         //
-        // Left:
-        // [0] = 1
-        // [1] = 1 * 1
-        // [2] = 2 * 1
-        // [3] = 3 * 2
-        //
-        // Right:
-        // [3] = 1
-        // [2] = 1 * 1
-        // [1] = 2 * 1
-        // [0] = 3 * 2
-        //
 
         int n = nums.size();
-        std::vector<int>arr;
-        std::vector<int>left[n]; // initilize vector size
-        std::vector<int>right[n]; // init vector size
+        std::vector<int>arr(n);  // initilize vector size
+        std::vector<int>left(n); // init vector size
+        std::vector<int>right(n);// init vector size
 
+        // Left:
+        // [0] = 1     = 1
+        // [1] = 1 * 1 = 1
+        // [2] = 2 * 1 = 2
+        // [3] = 3 * 2 = 6
+        //
         left[0] = 1;
         for(int i = 1; i<n; i++){
             left[i] = left[i-1] * nums[i-1];
+        }
+
+        // Right:
+        // [0] = 12 * 2 = 24
+        // [1] = 4  * 3 = 12
+        // [2] = 1  * 4 = 4
+        // [3] = 1      = 1
+        //
+        right[n-1] = 1;
+        for(int i = n-2; i >= 0; i--){
+            right[i] = right[i+1] * nums[i+1];
+        }
+
+        // Return:
+        // [0] = 1 * 24  = 24
+        // [1] = 1 * 12 = 12
+        // [2] = 2 * 4 = 8
+        // [3] = 6 * 1 = 6
+        //
+        for(int i = 0; i < n; i++){
+            arr[i]= right[i]*left[i];
         }
 
         return arr;
