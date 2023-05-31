bfed544150321aec69f32aca16dad1a4a95b2392
Marni <83829559+Flakvard@users.noreply.github.com>
2023-05-31 21:36:49 +0200

    Update Longest_Consecutive_Sequence.cpp

--- Arrays_&_Hashing/Longest_Consecutive_Sequence.cpp
+++ Arrays_&_Hashing/Longest_Consecutive_Sequence.cpp
@@ -1,1 +1,13 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        // Input: nums = [100,4,200,1,3,2]
+        // Output: 4
+        // Explanation: 
+        // The longest consecutive elements sequence is [1, 2, 3, 4]. 
+        // Therefore its length is 4.
 
+
+    }
+
+}
