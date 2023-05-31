e6bb9f94bc18405f0bd03bb8c2a7b9fbe84ef5d7
Marni <83829559+Flakvard@users.noreply.github.com>
2023-06-01 00:27:29 +0200

    Update Longest_Consecutive_Sequence.cpp

--- Arrays_&_Hashing/Longest_Consecutive_Sequence.cpp
+++ Arrays_&_Hashing/Longest_Consecutive_Sequence.cpp
@@ -6,7 +6,27 @@ public:
         // Explanation: 
         // The longest consecutive elements sequence is [1, 2, 3, 4]. 
         // Therefore its length is 4.
+        std::vector<int>sort_nums = nums;
+        std::sort(sort_nums.begin(), sort_nums.end());
+        int n = nums.size();
 
+        int largest_counter = 0;
+        int counter = 1;
+
+        for(int i = 0; i < n; i++){
+            //if (i == 0) continue;
+            if (counter > largest_counter){
+                largest_counter = counter;
+            }
+            
+            if (sort_nums[i+1]-sort_nums[i] == 1){
+                counter += 1;
+            }else{
+                counter = 1;
+                continue;
+            }
+        }
+        return largest_counter;
 
     }
 
